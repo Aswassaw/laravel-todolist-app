7749ffb5533bb441d1c5bd846805881b290037e0
Andry Pebrianto <andrypeb227@gmail.com>
2024-03-07 10:10:10 +0700

    Membuat route berdasarkan controller UserController

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    public function login() {
+
+    }
+
+    public function doLogin() {
+
+    }
+
+    public function doLogout() {
+
+    }
+}
