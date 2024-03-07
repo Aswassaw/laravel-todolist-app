faf62f71f62d1abcd36dd1d14efe5e30fe52b21c
Andry Pebrianto <andrypeb227@gmail.com>
2024-03-07 09:57:29 +0700

    Membuat route untuk template

--- tests/Feature/UserServiceTest.php
+++ tests/Feature/UserServiceTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Feature;
 
 use App\Services\UserService;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserServiceTest extends TestCase
