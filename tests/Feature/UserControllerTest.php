b4abba876ba40d5c3d0d90f7985400c91ec8bfeb
Andry Pebrianto <andrypeb227@gmail.com>
2024-03-13 15:22:12 +0700

    Create GuestMiddleware & MemberMiddleware

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -6,9 +6,16 @@ use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
-    public function testLogin()
+    public function testLoginPage()
     {
         $this->get("/login")->assertSeeText("Login");
+    }
+
+    public function testLoginPageForMember()
+    {
+        $this->withSession([
+            "username" => "andry"
+        ])->get("/login")->assertRedirect("/");
     }
 
     public function testLoginSuccess()
@@ -17,6 +24,16 @@ class UserControllerTest extends TestCase
             "username" => "andry",
             "password" => "aswassaw",
         ])->assertRedirect("/")->assertSessionHas("username", "andry");
+    }
+
+    public function testLoginForMember()
+    {
+        $this->withSession([
+            "username" => "andry"
+        ])->post("/login", [
+            "username" => "andry",
+            "password" => "aswassaw",
+        ])->assertRedirect("/");
     }
 
     public function testLoginValidationError()
@@ -37,5 +54,10 @@ class UserControllerTest extends TestCase
         $this->withSession([
             "username" => "andry",
         ])->post("/logout")->assertRedirect("/")->assertSessionMissing("username");
+    }
+
+    public function testLogoutGuest()
+    {
+        $this->post("/logout")->assertRedirect("/login");
     }
 }
