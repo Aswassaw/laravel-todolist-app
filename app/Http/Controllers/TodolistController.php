4905e95af6e40e67090fd0f6d489126d5648825f
Andry Pebrianto <andrypeb227@gmail.com>
2024-03-15 08:46:54 +0700

    Menampilkan semua todo

--- app/Http/Controllers/TodolistController.php
+++ app/Http/Controllers/TodolistController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TodolistService;
 use Illuminate\Http\Request;
 
 class TodolistController extends Controller
 {
+    private TodolistService $todolistService;
+
+    public function __construct(TodolistService $todolistService)
+    {
+        $this->todolistService = $todolistService;
+    }
+
     public function todoList(Request $request)
     {
+        return response()->view("todolist.todolist", [
+            "title" => "Todo List",
+            "todolist" => $this->todolistService->getTodoList(),
+        ]);
     }
 
     public function addTodo(Request $request)
